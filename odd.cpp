01df07811d0619ef4e6c39397ff21a359e9fc974
Maheshgollla <mahesh.golla072@gmail.com>
2025-02-01 14:16:57 +0530

    evenor odd

---
+++ odd.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"enter a number:";
+    cin>>x;
+
+    if(x %2==0)
+    {
+        cout<<"it is even number";
+    }
+    else
+    cout<<"it is odd number";
+}
